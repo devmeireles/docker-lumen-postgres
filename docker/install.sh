10f2ef0e5c3b5bbf7a2a7ed46e389de2c9589d20
Gabriel Meireles <dev.meireles@gmail.com>
2022-04-07 19:58:40 -0300

    Updating libraries

--- docker/install.sh
+++ docker/install.sh
@@ -2,6 +2,7 @@ apt update
 apt install -y git
 apt install -y zip
 apt install -y sqlite
+apt install -y php8.1-pgsql && php-common
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php composer-setup.php
 mv composer.phar /usr/local/bin/composer
